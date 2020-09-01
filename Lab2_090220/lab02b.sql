48e47df477ece6c49dcefb1db5803622a21fae7d
Ian Cowan <ian.cowan@centre.edu>
2020-09-01 14:58:54 +0000

    Fix whitespace in lab2

--- Lab2_090220/lab02b.sql
+++ Lab2_090220/lab02b.sql
@@ -1,12 +1,12 @@
 /*
-	Lab 02b: MariaDB Tutorial
-	CSC 362 Database Systems
-	Ian Cowan
+    Lab 02b: MariaDB Tutorial
+    CSC 362 Database Systems
+    Ian Cowan
 
-	This script creates the instructor table that can be found in the
-	DD textbook.
+    This script creates the instructor table that can be found in the
+    DD textbook.
 
-	Created 09/01/2020
+    Created 09/01/2020
 */
 
 /* Create the database and drop the previous version if it exists */
@@ -18,20 +18,20 @@ USE school;
 
 /* Create the table */
 CREATE TABLE instructors (
-	PRIMARY KEY (instructor_id),
-	instructor_id INT unsigned NOT NULL AUTO_INCREMENT,
-	inst_first_name VARCHAR(20) NOT NULL,
-	inst_last_name VARCHAR(20) NOT NULL,
-	campus_phone VARCHAR(8) NOT NULL
+    PRIMARY KEY (instructor_id),
+    instructor_id   INT unsigned NOT NULL AUTO_INCREMENT,
+    inst_first_name VARCHAR(20)  NOT NULL,
+    inst_last_name  VARCHAR(20)  NOT NULL,
+    campus_phone    CHAR(8)      NOT NULL
 );
 
 /* Insert the sample values from the textbook */
 INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone)
 VALUES
-	('Kira', 'Bently', '363-9948'),
-	('Timothy', 'Ennis', '527-4992'),
-	('Shannon', 'Black', '336-5992'),
-	('Estela', 'Rosales', '322-6992');
+    ('Kira', 'Bently', '363-9948'),
+    ('Timothy', 'Ennis', '527-4992'),
+    ('Shannon', 'Black', '336-5992'),
+    ('Estela', 'Rosales', '322-6992');
 
 /* Display the table */
 SELECT * FROM instructors;
