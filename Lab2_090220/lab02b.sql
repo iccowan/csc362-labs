05c994ce2d12a2b350e59a37037c277e8927a94d
Ian Cowan <ian.cowan@centre.edu>
2020-09-01 13:10:28 +0000

    Lab2

---
+++ Lab2_090220/lab02b.sql
@@ -0,0 +1,37 @@
+/*
+	Lab 02b: MariaDB Tutorial
+	CSC 362 Database Systems
+	Ian Cowan
+
+	This script creates the instructor table that can be found in the
+	DD textbook.
+
+	Created 09/01/2020
+*/
+
+/* Create the database and drop the previous version if it exists */
+DROP DATABASE IF EXISTS school;
+
+CREATE DATABASE school;
+
+USE school;
+
+/* Create the table */
+CREATE TABLE instructors (
+	PRIMARY KEY (instructor_id),
+	instructor_id INT unsigned NOT NULL AUTO_INCREMENT,
+	inst_first_name VARCHAR(20) NOT NULL,
+	inst_last_name VARCHAR(20) NOT NULL,
+	campus_phone VARCHAR(8) NOT NULL
+);
+
+/* Insert the sample values from the textbook */
+INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone)
+VALUES
+	('Kira', 'Bently', '363-9948'),
+	('Timothy', 'Ennis', '527-4992'),
+	('Shannon', 'Black', '336-5992'),
+	('Estela', 'Rosales', '322-6992');
+
+/* Display the table */
+SELECT * FROM instructors;
