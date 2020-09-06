c4e527d7aab5185369ffbe3bea5aa3b45fa4fcbf
Ian Cowan <ian.cowan@centre.edu>
2020-09-06 20:07:59 +0000

    Lab3 finish first part

--- Lab3_090720/lab3_sol.sql
+++ Lab3_090720/lab3_sol.sql
@@ -9,48 +9,92 @@
 *   Created 09/07/2020
 */
 
--- Drop the previous version of the database if it exists
+/* Drop the previous version of the database if it exists */
 DROP DATABASE IF EXISTS movie_ratings;
 
--- Create the database
+/* Create the database */
 CREATE DATABASE movie_ratings;
 
--- Use the appropriate database
+/* Use the appropriate database */
 USE movie_ratings;
 
--- Now, create the 3 tables that we need and their fields
--- movies Table
-CREATE TABLE movies(
-    PRIMARY KEY  (movie_id),
-    movie_id     INT(11)      NOT NULL UNSIGNED AUTO_INCREMENT,
-    movie_title  VARCHAR(256) NOT NULL,
-    release_date DATE         NOT NULL,
-    genre        VARCHAR(256) NOT NULL
+/* Now, create the 3 tables that we need and their fields */
+/* movies Table */
+CREATE TABLE movies (
+    PRIMARY KEY        (movie_id),
+    movie_id           INT(11) unsigned   NOT NULL AUTO_INCREMENT,
+    movie_title        VARCHAR(256) 	  NOT NULL,
+    movie_release_date DATE         	  NOT NULL,
+    movie_genre        VARCHAR(256) 	  NOT NULL
 );
 
--- consumers Table
-CREATE TABLE consumers(
+/* consumers Table */
+CREATE TABLE consumers (
     PRIMARY KEY (consumer_id),
-    consumer_id INT(11)      NOT NULL UNSIGNED AUTO_INCREMENT,
-    first_name  VARCHAR(64)  NOT NULL,
-    last_name   VARCHAR(64)  NOT NULL,
-    address     VARCHAR(128) NOT NULL,
-    city        VARCHAR(64)  NOT NULL,
-    state       CHAR(2)      NOT NULL,
-    zip         INT(5)       NOT NULL
+    consumer_id          INT(11) unsigned NOT NULL AUTO_INCREMENT,
+    consumer_first_name  VARCHAR(64)  	  NOT NULL,
+    consumer_last_name   VARCHAR(64)  	  NOT NULL,
+    consumer_address     VARCHAR(128) 	  NOT NULL,
+    consumer_city        VARCHAR(64)  	  NOT NULL,
+    consumer_state       CHAR(2)      	  NOT NULL,
+    consumer_zip         INT(5)      	  NOT NULL
 );
 
--- ratings Table
-CREATE TABLE ratings(
-    PRIMARY KEY  (movie_id, consumer_id),
-    movie_id     INT(11)    NOT NULL UNSIGNED,
-    FOREIGN KEY  (movie_id)
-                 REFERENCES movies (movie_id)
-                 ON DELETE RESTRICT,
-    consumer_id  INT(11)    NOT NULL UNSIGNED,
-    FOREIGN KEY  (consumer_id)
-                 REFERENCES consumers (consumer_id)
-                 ON DELETE RESTRICT,
-    number_stars TINYINT(1) NOT NULL UNSIGNED,
-    when_rated   DATE       NOT NULL
+/* ratings Table */
+CREATE TABLE ratings (
+    PRIMARY KEY         (movie_id, consumer_id),
+    movie_id            INT(11) unsigned     NOT NULL,
+    FOREIGN KEY         (movie_id)
+                        REFERENCES movies (movie_id)
+                        ON DELETE RESTRICT,
+    consumer_id         INT(11) unsigned     NOT NULL,
+    FOREIGN KEY         (consumer_id)
+                        REFERENCES consumers (consumer_id)
+                        ON DELETE RESTRICT,
+    rating_number_stars TINYINT(1) unsigned  NOT NULL,
+    when_rated          DATETIME      	     NOT NULL
 );
+
+/* Seed the movies database with values */
+INSERT INTO movies (movie_title, movie_release_date, movie_genre)
+VALUES ('The Hunt for Red October', '1990-03-02', 'Action, Adventure, Thriller'),
+       ('Lady Bird', '2017-12-01', 'Comedy, Drama'),
+       ('Inception', '2010-08-16', 'Action, Adventure, Sci-Fi');
+
+/* Seed the consumers table with values */
+INSERT INTO consumers (consumer_first_name, consumer_last_name,
+		       consumer_address, consumer_city, consumer_state, consumer_zip)
+VALUES ('Toru', 'Okada', '800 Glenridge Ave', 'Hobart', 'IN', 46342),
+       ('Kumiko', 'Okada', '864 NW Bohemia St', 'Vincetown', 'NJ', 08088),
+       ('Norobu', 'Wataya', '342 Joy Ridge St', 'Hermitage', 'TN', 37076),
+       ('May', 'Kasahara', '5 Kent Rd', 'East Haven', 'CT', 06512);
+
+/* Seed the ratings table with values */
+INSERT INTO ratings (movie_id, consumer_id, rating_number_stars, when_rated)
+VALUES (1, 1, 4, '2010-09-02 10:54:19'),
+       (1, 3, 3, '2012-08-05 15:00:01'),
+       (1, 4, 1, '2016-10-02 23:58:12'),
+       (2, 3, 2, '2017-03-27 00:12:48'),
+       (2, 4, 4, '2018-08-02 00:54:42');
+
+/* Preview the tables
+SELECT *
+  FROM movies;
+SELECT *
+  FROM consumers;
+SELECT *
+  FROM ratings; */
+
+/* Generate a report of the ratings using natural join */
+SELECT consumer_first_name, consumer_last_name, movie_title, rating_number_stars
+  FROM movies
+       NATURAL JOIN ratings
+       NATURAL JOIN consumers;
+
+/*
+*   The problem with the movies table at this point is that we have a multi-value
+*   field (genre field). We will fix this by using a subset table that will allow
+*   us to enter multiple genres for each movie.
+*/
+
+
