44c76ec55e1ae8e8c01625ca48447f71277a09b3
Ian Cowan <ian.cowan@centre.edu>
2020-10-04 17:19:15 +0000

    Finish lab8

--- Lab8_100320/trade_pkmn.sql
+++ Lab8_100320/trade_pkmn.sql
@@ -11,7 +11,7 @@ CREATE TABLE trainers (
 );
 
 CREATE TABLE pokemon (
-    PRIMARY KEY (pokemon_id),
+    PRIMARY KEY (pokemon_id, trainer_id),
     pokemon_id  VARCHAR(64) NOT NULL,
     trainer_id  INT(11) unsigned NOT NULL,
     FOREIGN KEY (trainer_id)
@@ -21,27 +21,50 @@ CREATE TABLE pokemon (
 /* Seed the tables with some sample data */
 INSERT INTO trainers (trainer_name)
 VALUES ('Ian'),
-       ('Patrick'),
-       ('Danny'),
-       ('TK'),
-       ('Sean'),
-       ('Newcomb'),
-       ('Drake'),
-       ('Owen');
+       ('Patrick');
 
 INSERT INTO pokemon (pokemon_id, trainer_id)
 VALUES ('poke1', 1),
-       ('poke2', 2),
-       ('poke3', 1),
-       ('poke4', 3);
+       ('poke2', 2);
 
-DELIMITER $$
+/* Create functions to get a trainer's id and set a trainer's id for pokemon */
+DROP FUNCTION IF EXISTS get_trainer_id;
 
 CREATE FUNCTION get_trainer_id(poke_id VARCHAR(64)) RETURNS INT
+RETURN (SELECT trainer_id
+          FROM pokemon
+         WHERE pokemon_id = poke_id);
+
+/* Create a function to trade pokemon between 2 trainers */
+DELIMITER //
+
+CREATE PROCEDURE trade_pokemon(IN pokemon1 VARCHAR(64), IN pokemon2 VARCHAR(64))
 BEGIN
-    RETURN (SELECT trainer_id
-             FROM pokemon
-            WHERE pokemon_id = poke_id);
-END $$
+    START TRANSACTION;
+        SET @pokemon1_trainer = (get_trainer_id(pokemon1));
+        SET @pokemon2_trainer = (get_trainer_id(pokemon2));
+
+        UPDATE pokemon
+           SET trainer_id = @pokemon1_trainer
+         WHERE pokemon_id = pokemon2;
+
+        UPDATE pokemon
+           SET trainer_id = @pokemon2_trainer
+         WHERE pokemon_id = pokemon1;
+    COMMIT;
+END //
 
 DELIMITER ;
+
+/* Let's show off our work */
+      SELECT pokemon_id, trainer_name
+        FROM trainers
+NATURAL JOIN pokemon;
+
+/* Trade poke1 and poke2 */
+CALL trade_pokemon('poke1', 'poke2');
+
+/* Show the results again */
+      SELECT pokemon_id, trainer_name
+        FROM trainers
+NATURAL JOIN pokemon;
